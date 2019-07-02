f1b83ea96a8e7efe4939e1582914ab0676b2a5af
Mohamed Amine EL ATTABI <mohamed.amine.elattabi@gmail.com>
2019-07-02 11:48:26 +0100

    sleep between 60 seconds and 5 to prevent block

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ async function actions(nb) {
     contacts = $("button[data-control-name=invite]") || false; //get all contacts
     if(contacts && contacts.length > 0 ){
       for(let i = 0; i < contacts.length; i++) {
-        ms = Math.floor(Math.random() * (20000 - 2000 + 1) + 2000); //get random ms to sleep between 20 seconds and 2
+        ms = Math.floor(Math.random() * (60000 - 5000 + 1) + 5000); //get random ms to sleep between 20 seconds and 2
         let contact = contacts[i].getAttribute("aria-label").split(" ");
         console.log(`inviting ${contact[1]} ${contact[2]}`); //write full name
         contacts[i].click(); //click connect button
@@ -31,12 +31,12 @@ async function invite(height, pages){
   console.warn(`Scroll number : ${pages}`);
   scroll(0, document.body.clientHeight);
   setTimeout(function() {
-		if(height != document.body.clientHeight && pages > 0){ //scroll
-			invite(document.body.clientHeight, --pages);
-		} else {
-      actions();
-    }
-	}, 1500);
+	if(height != document.body.clientHeight && pages > 0){ //scroll
+		invite(document.body.clientHeight, --pages);
+	} else {
+		actions();
+	}
+  }, 1500);
 }
 //to run invitation
 invite(document.body.clientHeight, nb_pages);
